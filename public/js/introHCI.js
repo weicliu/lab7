2519a7cdadd39eb3afa79b20440f2f1707f18eab
Weichen Liu <kevbtc@gmail.com>
2016-02-16 21:00:41 -0800

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,9 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(gaEvent);
+}
+
+function gaEvent() {
+	ga("send", "event", "like", "click");
 }
